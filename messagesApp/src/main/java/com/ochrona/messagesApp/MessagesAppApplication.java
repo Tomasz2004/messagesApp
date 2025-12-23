443796fea250ff608f3c4d30ab382a5a92810872
t-rogalski <tomaszrogalski04@gmail.com>
2025-12-23 17:12:39 +0100

    Init spring react

---
+++ messagesApp/src/main/java/com/ochrona/messagesApp/MessagesAppApplication.java
@@ -0,0 +1,13 @@
+package com.ochrona.messagesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesAppApplication.class, args);
+	}
+
+}
